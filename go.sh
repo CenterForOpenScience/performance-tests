0d38c42b239254d80acf9973eab0e886e5869f3a
Matt Clark <mattclark@cos.io>
2023-06-16 12:51:56 -0400

    Always pull docker image in go.sh to keep up to date

--- go.sh
+++ go.sh
@@ -1,3 +1,3 @@
 current_date=$( date '+%F_%H_%M_%S' )
-docker run -it -v ${PWD}/reports/report_$current_date:/opt/jmeter/reports quay.io/centerforopenscience/performance-tests
+docker run --pull=always -it -v ${PWD}/reports/report_$current_date:/opt/jmeter/reports quay.io/centerforopenscience/performance-tests
 echo "Test Results Are Available: ${PWD}/reports/report_$current_date"
